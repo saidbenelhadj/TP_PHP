d1a79549d5bf0c97da1f0204ab227ed09b9925b4
said benelhadj <sad.beneldje@gmail.com>
2023-06-15 14:05:03 +0200

    TP 02

--- TP02.php
+++ TP02.php
@@ -5,7 +5,7 @@ $num = 15;
 
 if($num < $seuil) {
     $resultat = $num * 2;
-    echo "La valeur de notre resultat $resultat est bien inférieur a notre seuil $seuil";
+    echo "La valeur de notre resultat $num est bien inférieur a notre seuil $seuil";
 }
 else{
     echo $num;
